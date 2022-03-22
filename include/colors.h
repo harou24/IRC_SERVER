44308cddbf00574f5b5162fabcdea48d4690687b
harou24 <haroutioun24@gmail.com>
2022-03-22 19:12:00 +0100

    colors.h for more style

---
+++ include/colors.h
@@ -0,0 +1,10 @@
+#ifndef COLORS_H
+# define COLORS_H
+
+#  define GREEN "\033[0;32m"
+#  define RED "\033[0;31m"
+#  define YELLOW "\033[0;33m"
+#  define BLUE "\033[0;34m"
+#  define RESET "\033[0m"
+
+#endif
