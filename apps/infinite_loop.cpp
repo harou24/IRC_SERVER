3d7777eb70e731a858371a29a79ff49851283a60
Haroutioun <haroutioun24@gmail.com>
2022-03-28 01:32:15 +0200

    Demo on How to stop a loop from another program ?

---
+++ apps/infinite_loop.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <signal.h>
+#include <unistd.h>
+
+int running = 1;
+
+void sig_handler(int pid)
+{
+  std::cout << "Stopping the loop... pid=" << pid << ".\n";
+  running = 0;  
+}
+
+int     main(void)
+{
+    signal(SIGQUIT, sig_handler);
+
+    std::cout << "Starting the loop...\n";
+    while (running)
+    {
+      std::cout << "looping...\n";
+      sleep(1);
+    }
+    std::cout << "Loop stoped!\n";
+    return (0);
+}
