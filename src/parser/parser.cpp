9fc52be0728ffcaab21b2b00f2f9fe0138dd751d
Eutienne Rodrigues <eutrodri@f1r6s13.codam.nl>
2022-03-27 19:24:19 +0200

    Set up a class, that parse the commands. Command: Away, Join, Me and MSG are working al ready

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -2,10 +2,86 @@
 
 #include <iostream>
 
+namespace Parser{
+
 Parser::Parser(void)
 { 
     std::cout << "Parser !\n";
 }
 
-
 Parser::~Parser(void) { }
+
+std::pair<CommandType, std::string> find_command(const std::string& s)
+    {
+        int         start = 1, end = s.find(" ");
+        std::string s1, s2;
+        CommandType command;
+        std::map<std::string,CommandType> string_to_case;
+        string_to_case.insert(std::make_pair<std::string,CommandType>("AWAY",AWAY)); 
+        string_to_case.insert(std::make_pair<std::string,CommandType>("JOIN",JOIN)); 
+        string_to_case.insert(std::make_pair<std::string,CommandType>("LIST",LIST)); 
+        string_to_case.insert(std::make_pair<std::string,CommandType>("NICK",NICK)); 
+        string_to_case.insert(std::make_pair<std::string,CommandType>("MSG",MSG));
+        string_to_case.insert(std::make_pair<std::string,CommandType>("INVITE",INVITE));
+        string_to_case.insert(std::make_pair<std::string,CommandType>("ME",ME));
+        s1 = s.substr(start, end - start);
+        s2 = s.substr(end +1, s.length() - (end +1));
+
+        for (std::string::iterator p = s1.begin(); s1.end() != p; ++p)
+            *p = toupper(*p);
+
+        if (string_to_case.find(s1) != string_to_case.end() && (end +1) <= s.length())
+            command = string_to_case.find(s1)->second;
+        else
+            command = UNKOWN;
+        return (std::make_pair(command, s2));
+    }
+
+    Token& Parser::parse(const std::string &inProgram)
+    {
+        Token   *currentToken = new Token();
+        currentToken->mRawText = inProgram;
+        std::pair<CommandType, std::string> split_str;
+        if (inProgram[0] == '/'){
+            split_str = find_command(inProgram);
+            switch(split_str.first){
+                case AWAY:
+                    currentToken->mType = AWAY;
+                    currentToken->mParser = new away(split_str.second);
+                    break;
+                case JOIN:
+                    currentToken->mParser = new join(split_str.second);
+                    if (currentToken->mParser->channel != "")
+                        currentToken->mType = JOIN;
+                    break;
+                case LIST:
+                    currentToken->mType = LIST;
+                    currentToken->mParser = new list(split_str.second);
+                    break;
+                case NICK:
+                    currentToken->mParser = new nick(split_str.second);
+                    currentToken->mType = NICK;
+                    break;
+                case MSG:
+                    currentToken->mParser = new msg(split_str.second);
+                    if (currentToken->mParser->name != "")    
+                        currentToken->mType = MSG;
+                    break;
+                case INVITE:
+                    currentToken->mParser = new invite(split_str.second);
+                    if (currentToken->mParser->name != "")    
+                        currentToken->mType = INVITE;
+                    break;
+                case ME:
+                    currentToken->mParser = new me(split_str.second);
+                    if (currentToken->mParser->message != "")    
+                        currentToken->mType = ME;
+                    break;
+                
+                default:
+                    break; 
+            }
+        }
+        return *currentToken;
+    }
+}
--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -1,12 +1,177 @@
 #ifndef PARSER_HPP
 # define PARSER_HPP
 
-class Parser {
-    private:
+#include <utility>
+#include <string>
+#include <vector>
+#include <map>
+#include <sstream>
 
-    public:
-        Parser(void);
-        ~Parser(void);
-};
+namespace Parser{
 
+    struct Iparse {
+
+        virtual ~Iparse(){}
+        std::string name;
+        std::string message;
+        std::string channel;
+        std::string newtopic;
+        size_t      min;
+        size_t      max;
+    };
+
+    struct away : public Iparse{
+        away(const std::string& x) {message = x;}
+        // std::string message;
+    };
+
+    struct invite : public Iparse{
+        invite(const std::string& x)
+        {
+            std::istringstream  ss(x);
+            ss >> name;
+            channel = x.substr(name.length()+1);
+            if (channel[0] != '#')
+            {
+                channel = "";
+                name = "";
+            }
+        }
+        // std::string name;
+        // std::string channel;
+    };
+
+    struct join : public Iparse{
+        join(const std::string& x) 
+        {
+            std::istringstream  ss(x);
+            ss >> channel;
+            ss >> message;
+            if (channel[0] != '#')
+                channel = "";
+        }
+        // std::string channel;
+    };
+
+    struct list : public Iparse{
+        list(const std::string& x){}
+        // std::string channel;
+        // size_t      min;
+        // size_t      max;
+    };
+
+    struct me : public Iparse{
+        me(const std::string& x) {message = x;}
+        // std::string message;
+    };
+
+    struct msg : public Iparse{
+        msg(const std::string& x) 
+        {
+            if (x.length() == 0)
+                return;
+            std::istringstream  ss(x);
+            ss >> name;
+            message = x.substr(name.length()+1);
+        }
+        // std::string name;
+        // std::string message;
+    };
+
+    struct nick : public Iparse{
+        nick(const std::string& x) {name = x;}
+        // std::string name;
+    };
+
+    struct notice : public Iparse{
+        notice(const std::string& x) {name = x;}
+        // std::string name;
+        // std::string message;
+    };
+
+    struct part : public Iparse{
+        part(const std::string& x) {name = x;}
+        // std::string channel;
+    };
+
+    struct privmsg : public Iparse{
+        privmsg(const std::string& x) {name = x;}
+        // std::string name;
+        // std::string message;
+    };
+
+    struct query : public Iparse{
+        query(const std::string& x) {name = x;}
+        // std::string name;
+        // std::string message;
+    };
+
+    struct quit : public Iparse{
+        quit(const std::string& x) {message = x;}
+        // std::string message;
+    };
+
+    struct topic : public Iparse{
+        topic(const std::string& x) {name = x;}
+        // std::string channel;
+        // std::string newtopic;
+    };
+
+    struct whois : public Iparse{
+        whois(const std::string& x) {name = x;}
+        // std::string name;
+    };
+
+    struct kick : public Iparse{
+        kick(const std::string& x) {name = x;}
+        // std::string channel;
+        // std::string name;
+    };
+
+
+    enum CommandType {
+        AWAY,
+        INVITE,
+        JOIN,
+        LIST,
+        ME,
+        MSG,
+        NICK,
+        NOTICE,
+        PART,
+        PRIVMSG,
+        QUERY,
+        QUIT,
+        TOPIC,
+        WHOIS,
+        KICK,
+        MODE,
+        USER,
+        UNKOWN
+    };
+
+    struct Token{
+
+        Token() : mType(UNKOWN), mParser(0), mId(""), mRawText("") {}
+
+        Token(const std::string& x) : mType(UNKOWN), mParser(0), mId(""), mRawText(x) {}
+        Token(const std::string& x, std::string id) : mType(UNKOWN), mParser(0), mId(id), mRawText(x) {}
+
+
+        CommandType     mType;
+        Iparse          *mParser;
+        std::string     mId;
+        std::string     mRawText;
+    };
+
+    class Parser {
+        private:
+
+        public:
+            Parser(void);
+            ~Parser(void);
+            Token& parse(const std::string &inProgram);
+    };
+
+}
 #endif
