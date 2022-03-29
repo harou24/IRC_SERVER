f85a406b91a5d14f33be0ffb39d492c6ce47c56a
harou24 <haroutioun24@gmail.com>
2022-03-29 16:25:58 +0200

    MultiClientHandler

---
+++ src/MultiClientHandler/MultiClientHandler.cpp
@@ -0,0 +1,46 @@
+#include "MultiClientHandler.hpp"
+
+MultiClientHandler::MultiClientHandler(void)
+{
+    this->zeroFdSet();
+    this->fdMax = 0;
+}
+
+MultiClientHandler::~MultiClientHandler(void) { }
+
+void    MultiClientHandler::zeroFdSet(void)
+{
+    FD_ZERO(&this->mainFds);
+    FD_ZERO(&this->tmpFds);
+}
+
+void    MultiClientHandler::addFdToSet(int fd)
+{
+    FD_SET(fd, &this->mainFds);
+    this->fdMax += 1;
+}
+
+void    MultiClientHandler::clearFd(int fd)
+{
+    FD_CLR(fd, &this->mainFds);
+}
+
+bool    MultiClientHandler::isFdInSet(int fd)
+{
+    this->updateFdSet();
+    return (FD_ISSET(fd, &this->tmpFds) != 0);
+}
+
+
+void    MultiClientHandler::updateFdSet(void)
+{
+    this->tmpFds = this->mainFds;
+    if (select(this->fdMax + 1, &this->tmpFds, NULL, NULL, NULL) == - 1)
+        throw new UpdateFailed();
+}
+
+bool    MultiClientHandler::isFdReadyToCommunicate(int fd)
+{
+    return this->isFdInSet(fd);
+}
+
