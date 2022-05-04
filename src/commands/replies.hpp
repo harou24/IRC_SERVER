b04a9c0bbf13731ba74810d20a9378004497481f
Eutienne Rodrigues <eutrodri@f1r3s16.codam.nl>
2022-05-04 19:23:42 +0200

    Removed all hardcode stuf from host to HOST

--- src/commands/replies.hpp
+++ src/commands/replies.hpp
@@ -1,13 +1,12 @@
 #ifndef REPLIES_HPP
 # define REPLIES_HPP
 
+# define NOTICE(server) server + " NOTICE * :*** Looking up your hostname...\n"
 
-#define NOTICE(server) server + " NOTICE * :*** Looking up your hostname...\n"
-
-#define RPL_WELCOME(nick, args) ":" + args.arg3 + " 001 " + nick + "\n: Welcome to the Internet Relay Network\n" + nick + "!" + args.arg1 + "@" + args.arg2 + "\n"
-#define RPL_YOURHOST(args) ":Your host is " + args.arg3 + ", running version 1.0\n"
-#define RPL_CREATED ":This server was created 12.54.22 Apr 06 2022\n"
-#define LOGO_P1 "          _          _            _                     \n \
+# define RPL_WELCOME(nick, args) ":" + args.arg3 + " 001 " + nick + "\n: Welcome to the Internet Relay Network\n" + nick + "!" + args.arg1 + "@" + args.arg2 + "\n"
+# define RPL_YOURHOST(args) ":Your host is " + args.arg3 + ", running version 1.0\n"
+# define RPL_CREATED ":This server was created 12.54.22 Apr 06 2022\n"
+# define LOGO_P1 "          _          _            _                     \n \
         /\\ \\       /\\ \\        /\\ \\     \n\
          \\ \\ \\     /  \\ \\      /  \\ \\    \n\
          /\\ \\_\\   / /\\ \\ \\    / /\\ \\ \\   \n\
@@ -18,19 +17,19 @@
  ___/ / /__  / / /\\ \\ \\  / / /________  \n\
 /\\__\\/_/___\\/ / /  \\ \\ \\/ / /_________\\  \n\
 \\/_________/\\/_/    \\_\\/\\/____________/   \n\n"
-#define LOGO_P2 "                            _ _  \n \
+
+# define LOGO_P2 "                            _ _  \n \
        //||\\\\   ///||\\   /.-. | \n\
         ` @ @\\     a a   ||o_o|| \n\
            >        _)   || = || \n\
            -        ~      ' '   \n\
        haroutioun eutienne evita\n\n"
 
-#define RPL_UMODEIS(host, args) ":" + args.arg1 + "!" + args.arg1 + "@" + host + " MODE " + args.arg1 + " :" + args.arg2 +  "\n"
-// #define PRIV_MESSAGE(nick, nick2, host, message) ":" + nick + "!" + nick + "@" + host + "\n*" + nick + "* " + &message[1] + "\n"
-#define PRIV_MESSAGE(nick, nick2, user, host, message) ":" + nick + "!" + user + "@" + host + " PRIVMSG " + nick2 + " " + message + "\n"
-#define RPL_AWAY(nick, nick2, msg) ":127.0.0.1 301 " + nick2 + " " + nick + " " + msg + "\n"
-#define RPL_UNAWAY() ":127.0.0.1 305 :You are no longer marked as being away\n"
-#define RPL_NOAWAY() ":127.0.0.1 306 :You have been marked as being away\n"
-#define ERR_NOSUCHNICK(nick, nick2) ":127.0.0.1 401 " + nick + " " + nick2 + " :No such nick/channel\n"
+# define RPL_UMODEIS(host, args) ":" + args.arg1 + "!" + args.arg1 + "@" + host + " MODE " + args.arg1 + " :" + args.arg2 +  "\n"
+# define PRIV_MESSAGE(nick, nick2, user, host, message) ":" + nick + "!" + user + "@" + host + " PRIVMSG " + nick2 + " " + message + "\n"
+# define RPL_AWAY(nick, nick2, msg) ":" + std::string(HOST) + " 301 " + nick2 + " " + nick + " " + msg + "\n"
+# define RPL_UNAWAY() ":" + std::string(HOST) + " 305 :You are no longer marked as being away\n"
+# define RPL_NOAWAY() ":" + std::string(HOST) + " 306 :You have been marked as being away\n"
+# define ERR_NOSUCHNICK(nick, nick2) ":127.0.0.1 401 " + nick + " " + nick2 + " :No such nick/channel\n"
 
 #endif
