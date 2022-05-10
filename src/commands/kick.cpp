7c0dcd80d278ee0e2474795b16f7368decde4f23
EvitaO <eovertoo@student.codam.nl>
2022-05-10 17:21:02 +0200

    kick working :)

--- src/commands/kick.cpp
+++ src/commands/kick.cpp
@@ -2,22 +2,19 @@
 
 std::string     kick(const CmdController& controller)
 {
-    std::cout << "KICK\n";
     Client *cl = controller.getServer().getClientByStream(controller.getCurrentMsg().getStreamPtr());
     std::string channel_name = controller.getParser().getArgument().arg1;
     if (controller.getServer().isChannel(channel_name))
     {
-        Channel channel = controller.getServer().getChannel(channel_name);
-        if (channel.isOperator(*cl))
+        Channel *channel = &controller.getServer().getChannel(channel_name);
+        if (channel->isOperator(*cl))
         {
-            if (channel.isInChannel(controller.getParser().getArgument().arg2))
+            if (channel->isInChannel(controller.getParser().getArgument().arg2))
             {
-                std::string reply = RPL_KICK(cl, channel.getChannelName(), controller.getParser().getArgument().arg2, controller.getParser().getArgument().arg3);
-                channel.removeClient(*controller.getServer().getClientByName(controller.getParser().getArgument().arg2), reply);
-                controller.getServer().getClientByName(controller.getParser().getArgument().arg2)->getStream().send(reply, reply.length());
-
-                // std::string reply_kicked = RPL_PART(controller.getServer().getClientByName(controller.getParser().getArgument().arg2), channel_name);
-                // controller.getServer().getClientByName(controller.getParser().getArgument().arg2)->getStream().send(reply_kicked, reply_kicked.length());
+                std::string reply = RPL_KICK(cl, channel->getChannelName(), controller.getParser().getArgument().arg2, controller.getParser().getArgument().arg3);
+                Client *clientToKick = controller.getServer().getClientByName(controller.getParser().getArgument().arg2);
+                channel->removeClient(*clientToKick, reply);
+                clientToKick->getStream().send(reply, reply.length());
                 return "";
             }
             else if (controller.getServer().isNickInUse(controller.getParser().getArgument().arg2))
