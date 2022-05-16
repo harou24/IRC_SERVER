4095c53362cfeb316a62ee3790cf750e53b3910d
Haroutioun <haroutioun24@gmail.com>
2022-05-16 20:13:53 +0200

    added a test to check if the port number is valide

--- apps/irc/irc_server.cpp
+++ apps/irc/irc_server.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#include <limits.h>
+
 int     main(int argc, char **argv)
 {
     (void)argv;
@@ -12,7 +14,14 @@ int     main(int argc, char **argv)
         exit(1);
     }
 
-    IrcServer   S(atoi(argv[1]), std::string(argv[2]));
+    int port = atoi(argv[1]);
+    if (!(port > 1000 && port < INT_MAX))
+    {
+        std::cout << "The given port is not valide, it needs to be above 1000 and under MAX_INT\n";
+        exit(1);
+    }
+
+    IrcServer   S(port, std::string(argv[2]));
 
     S.start();
     return 0;
